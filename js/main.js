d849e3c93f334a5a4d64dad2f45177ee3566f039
Alisa <filonenkoalisa@gmail.com>
2021-04-28 15:10:23 +0200

    Primero parte de codigo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-console.log('Hello, World!');
 
+const coders = ['Alisa', 'Angie', 'Gaby', 'Rosangely', 'Yeraldine', 'Eliana', 'Inga', 'DanielaR', 'MariaLaura', 'Taiza', 'Nicole', 'AnaLissette', 'AnaGarcía', 'DanielaT', 'Marta', 'Amanda', 'Yasmis', 'Sònia', 'Leo', 'Pepi', 'Claudia', 'Jolga', 'Flo', 'Ranju'];
+
+
+
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
+
+const getRandomName = () =>
+  `${coders[getRandomNumber(coders.length)]}`;
+
+const setRandomName = () => {
+  document.getElementById('random-name').innerText = getRandomName();
+}
+
+document.getElementById('generate')
+  .addEventListener('click', setRandomName);
+
+setRandomName();
 
